6f34a8f75a12175d03d2c3ac189af96de23b0451
andfriend12345 <phamthithuykb2@gmail.com>
2025-08-29 13:03:51 +0700

    Update AntiLava.lua

--- AntiLava.lua
+++ AntiLava.lua
@@ -1,38 +1,41 @@
-local player = game.Players.LocalPlayer  
+pcall(function()
+    local player = game.Players.LocalPlayer  
 
-local function AntiLava(character)
-    if not character then return end  
-    character:WaitForChild("Humanoid")  
+    local function AntiLava(character)
+        if not character then return end  
+        character:WaitForChild("Humanoid")  
 
-    task.delay(2, function()
-        pcall(function()
-            sethiddenproperty(player, "DFlintTouchSenderMaxBandWidthBps", -1)
-            sethiddenproperty(player, "DFlintPhysicsSenderMaxBandWidthBps", -1)
-        end)
+        task.delay(2, function()
+            pcall(function()
+                sethiddenproperty(player, "DFlintTouchSenderMaxBandWidthBps", -1)
+                sethiddenproperty(player, "DFlintPhysicsSenderMaxBandWidthBps", -1)
+            end)
 
-        for _, v in pairs(character:GetDescendants()) do  
-            if v:IsA("BasePart") and v.Name ~= "HumanoidRootPart" then  
-                v.CanCollide = false  
-                v.CanTouch = false  
+            for _, v in pairs(character:GetDescendants()) do  
+                if v:IsA("BasePart") and v.Name ~= "HumanoidRootPart" then  
+                    v.CanCollide = false  
+                    v.CanTouch = false  
+                end  
             end  
-        end  
 
-        for _, module in pairs(game:GetDescendants()) do
-            if module:IsA("ModuleScript") and string.find(module.Name:lower(), "hitbox") then
-                module:Destroy()
+            for _, module in pairs(game:GetDescendants()) do
+                if module:IsA("ModuleScript") and string.find(module.Name:lower(), "hitbox") then
+                    module:Destroy()
+                end
             end
-        end
 
-        game.StarterGui:SetCore("SendNotification", {  
-            Title = "🌋 Anti lava!";  
-            Text = "Kích hoạt thành công!";  
-            Duration = 5;  
-        })
-    end)
-end
+            game.StarterGui:SetCore("SendNotification", {  
+                Title = "🌋 Anti lava!";  
+                Text = "Kích hoạt thành công!";  
+                Duration = 5;  
+            })
+        end)
+    end
 
-if player.Character then
-    AntiLava(player.Character)
-end  
+    if player.Character then
+        AntiLava(player.Character)
+    end  
 
+    player.CharacterAdded:Connect(AntiLava)
+end)
 player.CharacterAdded:Connect(AntiLava)
